635b1a43a33fdda9696307ccec1f3be9d8e1b52f
aditya gupta <eddy8700@gmail.com>
2016-11-07 09:35:51 +0530

    committing changes

--- restaurant-api/src/main/java/com/demo/restaurant/api/RestaurantService.java
+++ restaurant-api/src/main/java/com/demo/restaurant/api/RestaurantService.java
@@ -1,12 +1,12 @@
 package com.demo.restaurant.api;
 
 import java.io.File;
-import java.io.InputStream;
+import java.io.FileNotFoundException;
 
 public interface RestaurantService {
 	
 
-public Long findMaximumSatisfaction(File file);
+public Long findMaximumSatisfaction(File file) throws FileNotFoundException;
 
 
 }
