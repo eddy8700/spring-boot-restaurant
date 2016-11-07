f5460a13dc0f533a45dd48b38e00b11170c45cb1
aditya gupta <eddy8700@gmail.com>
2016-11-07 09:33:03 +0530

    committing changes

--- restaurant-rest/src/main/java/com/demo/restaurant/controller/RestaurantController.java
+++ restaurant-rest/src/main/java/com/demo/restaurant/controller/RestaurantController.java
@@ -3,13 +3,16 @@
  */
 package com.demo.restaurant.controller;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,26 +25,26 @@ import com.demo.restaurant.api.RestaurantService;
  */
 @RestController
 public class RestaurantController {
-	
-	private static final Logger LOGGER =LoggerFactory.getLogger(RestaurantController.class);
-	
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RestaurantController.class);
+
 	@Autowired
 	private RestaurantService restaurantService;
-	
-	
-	@RequestMapping(value="/findMaximumSatisfaction",method=RequestMethod.GET)
-	public Long findMaximumSatisfaction(){
+
+	@Value("${file.name}")
+	private String fileName;
+
+	@Autowired
+	private ResourceLoader resourceLoader;
+
+	@RequestMapping(value = "/findMaximumSatisfaction", method = RequestMethod.GET)
+	public Long findMaximumSatisfaction() throws IOException {
 		LOGGER.info("Loading the file from classpath resource");
-		Resource classpathResource= new  ClassPathResource("data.txt");
+		final Resource resource = resourceLoader.getResource("classpath:" + fileName);
+		final File readFromFile = resource.getFile();
 		LOGGER.info("Parsing the file");
-		try {
-			 return restaurantService.findMaximumSatisfaction(classpathResource.getFile());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-		
+		return restaurantService.findMaximumSatisfaction(readFromFile);
+
 	}
 
 }
